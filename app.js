b3681eb78491b6d9d47f86a11cd335584d1d57e3
Richard Lasco <lasco9@gmail.com>
2022-03-08 11:55:38 -0600

    Added main calculator logic to app.js

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+let displayPara = document.querySelector('.numbers');
+let numButtons = document.querySelectorAll(".number-button");
+let clearButton = document.querySelector('.clear-button');
+let divideButton = document.querySelector('.division-button');
+let multiplyButton = document.querySelector('.multiply-button');
+let subtractButton = document.querySelector('.subtract-button');
+let addButton = document.querySelector('.addition-button');
+let equalButton = document.querySelector('.equal-button');
+let numArray = [];
+
 function add(a, b) {
     return a + b;
 }; 
@@ -17,3 +27,56 @@ function divide(a, b) {
 function operate(operator, a, b) {
     return operator(a, b);
 }
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+numButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        displayPara.textContent += button.innerText;
+        console.log(button.innerText);
+    });
+});
+
+clearButton.addEventListener('click', () => {
+    removeAllChildNodes(displayPara);
+    numArray = [];
+    console.log(numArray);
+})
+
+divideButton.addEventListener('click', () => {
+    numArray.push(parseInt(displayPara.textContent), 'divide');
+    removeAllChildNodes(displayPara);
+    console.log(numArray);
+})
+
+multiplyButton.addEventListener('click', () => {
+    numArray.push(parseInt(displayPara.textContent), 'multiply');
+    removeAllChildNodes(displayPara);
+    console.log(numArray);
+})
+
+subtractButton.addEventListener('click', () => {
+    numArray.push(parseInt(displayPara.textContent), 'subtract');
+    removeAllChildNodes(displayPara);
+    console.log(numArray);
+})
+
+addButton.addEventListener('click', () => {
+    numArray.push(parseInt(displayPara.textContent), 'add');
+    removeAllChildNodes(displayPara);
+    console.log(numArray);
+})
+
+equalButton.addEventListener('click', () => {
+    numArray.push(parseInt(displayPara.textContent));
+    let result = operate(eval(numArray[1]), numArray[0], numArray[2]);
+    displayPara.textContent = result;
+    numArray = [];
+})
+
+
+
